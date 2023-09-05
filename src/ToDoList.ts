42c280aa7020199a6aa92caa76eeb6dc337f5e0a
flor <florvllb33@gmail.com>
2023-09-05 16:31:30 -0300

    primer actividad ts

--- src/ToDoList.ts
+++ src/ToDoList.ts
@@ -1,31 +1,31 @@
 class Tarea {
-  nombre: string;
-  fechaVencimiento: Date;
-  completada: boolean;
+  nombre: string
+  fechaVencimiento: Date
+  completada: boolean
 
-  constructor(nombre: string, fechaVencimiento: Date) {
-    this.nombre = nombre;
-    this.fechaVencimiento = fechaVencimiento;
-    this.completada = false; // Por defecto, la tarea no está completada
+  constructor (nombre: string, fechaVencimiento: Date) {
+    this.nombre = nombre
+    this.fechaVencimiento = fechaVencimiento
+    this.completada = false // Por defecto, la tarea no está completada
   }
 
-  completarTarea() {
-    this.completada = true;
+  completarTarea () {
+    this.completada = true
   }
 
-  descompletarTarea() {
-    this.completada = false;
+  descompletarTarea () {
+    this.completada = false
   }
 }
 
 // Ejemplo de uso
-const tarea1 = new Tarea("Hacer la compra", new Date("2023-09-30"));
-const tarea2 = new Tarea("Estudiar para el examen", new Date("2023-10-05"));
+const tarea1 = new Tarea('Hacer la compra', new Date('2023-09-30'))
+const tarea2 = new Tarea('Estudiar para el examen', new Date('2023-10-05'))
 
 // Completar una tarea
-tarea1.completarTarea();
+tarea1.completarTarea()
 
 // Descompletar una tarea
-tarea1.descompletarTarea();
+tarea2.completarTarea()
 
 // Agregar métodos para eliminar tareas si es necesario
