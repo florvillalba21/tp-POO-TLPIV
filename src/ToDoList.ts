9907908f0ce94abf7ea0fb38a5e88322469be107
flor <florvllb33@gmail.com>
2023-09-05 16:26:38 -0300

    primer actividad en process

---
+++ src/ToDoList.ts
@@ -0,0 +1,31 @@
+class Tarea {
+  nombre: string;
+  fechaVencimiento: Date;
+  completada: boolean;
+
+  constructor(nombre: string, fechaVencimiento: Date) {
+    this.nombre = nombre;
+    this.fechaVencimiento = fechaVencimiento;
+    this.completada = false; // Por defecto, la tarea no está completada
+  }
+
+  completarTarea() {
+    this.completada = true;
+  }
+
+  descompletarTarea() {
+    this.completada = false;
+  }
+}
+
+// Ejemplo de uso
+const tarea1 = new Tarea("Hacer la compra", new Date("2023-09-30"));
+const tarea2 = new Tarea("Estudiar para el examen", new Date("2023-10-05"));
+
+// Completar una tarea
+tarea1.completarTarea();
+
+// Descompletar una tarea
+tarea1.descompletarTarea();
+
+// Agregar métodos para eliminar tareas si es necesario
